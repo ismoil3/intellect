6f0e05453b4e1f00bf1afa04e7ce267cd9944f6b
ismoil3 <nasridinovismoil945@gmail.com>
2025-01-22 23:00:12 +0500

    add Reviews

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutIntellect from './components/aboutIntellect/aboutIntellect'
 import HeroSection from './components/hero-section/hero-section'
+import Reviews from './components/reviews/reviews'
 
 const page = () => {
   return (
     <div>
       <HeroSection/>
       <AboutIntellect/>
+      <Reviews/>
     </div>
   )
 }
